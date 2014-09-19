858f3eeb7b4382df18b23df8b5aafb2843cf2925
Allie Del Giorno <adelgior@cs.cmu.edu>
2014-09-19 17:34:51 -0400

    init

---
+++ logparse.py
@@ -0,0 +1,39 @@
+import os, sys, pdb, numpy
+
+def calculate_u(odom_arr, idx_first, idx_second):
+    obs_first = odom_arr[idx_first, :]
+    obs_second = odom_arr[idx_second, :]
+    u = obs_second - obs_first
+    return u
+
+def main():
+    fn = 'log/robotdata1.log'
+    lines = open(fn, 'r').readlines()
+    odoms = {}
+    
+    for (count, line) in enumerate(lines):
+        if line[0] == 'O':
+            odoms[count] = line.rstrip().split(' ')[1:]
+
+    print "entry length: {} ".format(len(odoms.values()[0]))
+    odom_arr = numpy.zeros((len(odoms), len(odoms.values()[0])), dtype = numpy.float64)
+    
+    for (idx, (k, obs)) in enumerate(odoms.items()):
+        odom_arr[idx, :] = obs
+
+    u_s = numpy.zeros_like(odom_arr)
+    for t in range(odom_arr.shape[0] -1):
+        u = calculate_u(odom_arr, t, t+1)
+        u_s[t, :] = u
+
+    print "max x, min x: {}".format((odom_arr[:, 0].max(), odom_arr[:, 0].min()))
+    print "max y, min y: {}".format((odom_arr[:, 1].max(), odom_arr[:, 1].min()))
+    print "mean x: {}".format(odom_arr[:, 1].mean())
+    print "max theta, min theta: {}".format((odom_arr[:, 2].max(), odom_arr[:, 2].min()))
+
+    print "xdiff: {}".format(odom_arr[:, 0].sum())
+
+    pdb.set_trace()
+
+if __name__ == '__main__':
+    main()
